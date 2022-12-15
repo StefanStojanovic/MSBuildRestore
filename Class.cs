2501c6eb101ebcb416070b8bdd54468b976c3bc8
StefanStojanovic <stefan.stojanovic@janeasystems.com>
2022-12-15 12:08:55 +0100

    Made a solution with two projects

---
+++ Class.cs
@@ -0,0 +1,12 @@
+﻿using MathNet.Numerics;
+
+namespace ClassLibrary
+{
+    public static class Class
+    {
+        public static double MultiplyByE(double x)
+        {
+            return x * Constants.E;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using ClassLibrary;
+using Newtonsoft.Json;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double value = Class.MultiplyByE(5);
+            string json = JsonConvert.SerializeObject(new { value });
+            Console.WriteLine(json);
+        }
+    }
+}
